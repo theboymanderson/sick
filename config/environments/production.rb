763572f00ea1a42cdec4ddfe3e49ceb9d0577507
The Boy <theboy@Matthews-MacBook-Pro.local>
2017-11-10 22:26:34 -0800

    email fix?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { :host => 'istheboysick.herokuapp.com' }
+  config.action_mailer.smtp_settings = { address: 'istheboysick.herokuapp.com' }
 end
