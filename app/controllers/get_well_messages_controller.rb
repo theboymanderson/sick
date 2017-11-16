181460743d53973cd445ae8b9e9acace2aa05d93
The Boy <theboy@Matthews-MacBook-Pro.local>
2017-11-15 22:46:46 -0800

    improve error msgs a little. icon in nav

--- app/controllers/get_well_messages_controller.rb
+++ app/controllers/get_well_messages_controller.rb
@@ -8,7 +8,9 @@ class GetWellMessagesController < ApplicationController
     email = params[:get_well_message][:user][:email].downcase
     message = params[:get_well_message][:message]
     user = User.find_by_email(email)
-    if user.authenticate(password)
+    if !user.present?
+      flash[:alert] = "Uh oh. That's not the email you signed up with!"
+    elsif user.authenticate(password)
       GetWellMessage.create(user: user, message: message)
     else
       flash[:alert] = "Nice try. But that ain't a valid p-word"
